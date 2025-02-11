0a7ecaaa7d23c9247fe9da564d1f22aa34d96164
DarkCode <darkcodeit@gmail.com>
2025-02-11 14:13:35 -0800

    import img

--- src/components/header/HeaderSlider.jsx
+++ src/components/header/HeaderSlider.jsx
@@ -1,16 +1,28 @@
-// import { Swiper, SwiperSlide } from 'swiper/react';
+// Import necessary modules
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper-bundle.css'; // Import Swiper styles
 
 export default function HeaderSlider() {
+    // Array of image paths in the public folder
+    const images = [
+        '/film1.jpg',
+        '/film2.jpg',
+        '/film3.jpg',
+        '/film4.jpg'
+    ];
     return (
         <div className="mt-10">
-            {/* <Swiper spaceBetween={50} slidesPerView={4} autoplay={{ delay: 200 }} loop>
-                {<SwiperSlide>
-                </SwiperSlide>}
-            </Swiper> */}
-
-
-
-     
+            <Swiper
+                spaceBetween={50}
+                slidesPerView={4}
+                autoplay={{ delay: 100 }}
+                loop>
+                {images.map((image, index) => (
+                    <SwiperSlide key={index}>
+                        <img src={image} alt={`Slide ${index + 1}`} className="w-full h-auto" />
+                    </SwiperSlide>
+                ))}
+            </Swiper>
         </div>
     );
 }
